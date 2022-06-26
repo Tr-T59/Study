a8a583c6dc4a78d6ac673ca871257563d75afce7
Tr-T59 <wrk.ivasishin@gmail.com>
2022-06-26 04:45:28 +0400

    added some spicy things

---
+++ phonebook-obj.h
@@ -0,0 +1,15 @@
+//phonebook header
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Phonebook
+{
+public:
+    string signature;
+    string home_number;
+    string work_number;
+    string personal_number;
+};
